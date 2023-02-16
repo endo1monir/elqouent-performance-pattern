9ac3e64eb603cc283a9f7f5ab7e0fa3026551aa0
endo1monir <andrew.monir@yahoo.com>
2023-02-16 08:01:22 +0200

    count aggreate function in pattern way

--- app/Http/Controllers/FeaturesController.php
+++ app/Http/Controllers/FeaturesController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 class FeaturesController extends Controller
 {
     public function index(){
+        $statuses=Feature::toBase()
+            ->selectRaw("count(case when status='Requested' then 1 end) as requested")
+            ->selectRaw("count(case when status='Planned' then 1 end) as planned")
+            ->selectRaw("count(case when status='Completed' then 1 end) as completed")
+            ->first();
         $features=Feature::withCount('comments')->paginate();
         return view('features',get_defined_vars());
     }
