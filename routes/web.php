151b38fb6cba63b0f09d43300a23a1158ae56924
endo1monir <andrew.monir@yahoo.com>
2023-02-21 08:17:13 +0200

    all

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use App\Http\Controllers\PostController;
 Route::get('posts',[PostController::class,'index']);
 Route::get('features',[\App\Http\Controllers\FeaturesController::class,'index']);
 Route::get('/features/{feature}',[\App\Http\Controllers\FeaturesController::class,'show']);
+Route::get('users');
 Route::get('/', function () {
     return view('welcome');
 });
